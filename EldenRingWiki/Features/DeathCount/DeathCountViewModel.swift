e909966f04e6cb01b146654245a77dd68f4427a3
Dmitry Shliugaev <shegodmal@gmail.com>
2022-12-03 20:01:00 +0300

    Save death count in UserDefault

--- EldenRingWiki/Features/DeathCount/DeathCountViewModel.swift
+++ EldenRingWiki/Features/DeathCount/DeathCountViewModel.swift
@@ -6,14 +6,22 @@
 //
 
 import Combine
+import Foundation
 
 class DeathCountViewModel: ObservableObject {
     private let repository: DeathRepositoryProtocol
+    private var cancellable: AnyCancellable?
     
     @Published var deathCount: Int
     
     init(repository: DeathRepositoryProtocol) {
         self.repository = repository
         deathCount = repository.getDeathCount()
+        
+        cancellable = $deathCount
+            .debounce(for: .milliseconds(300), scheduler: DispatchQueue.global(qos: .background))
+            .sink { count in
+                repository.saveDeathCount(count)
+            }
     }
 }
